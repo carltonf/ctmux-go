f1cdeafdcf077517f523409c05367b6a2c571eb1
Carl Xiong <xiongc05@gmail.com>
2024-10-04 12:49:41 +0800

    add win32 support
    - syscall.exec is not supported on Windows
    - add reset subcommand
    - misc tweaks

--- main.go
+++ main.go
@@ -3,60 +3,85 @@ package main
 import (
 	"embed"
 	"fmt"
-    "io/ioutil"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
+	"runtime"
 	"syscall"
-	"path/filepath"
 )
 
 //go:embed center.conf hub.conf node.conf
 var configFiles embed.FS
 
+const cacheDir = ".cache"
+
 // TODO ftype: conf, sock
 func getPath(fname string, ftype string) string {
-	const cacheDir = ".cache"
 	if err := os.MkdirAll(cacheDir, 0755); err != nil {
 		panic(err)
 	}
-	tempFilePath := filepath.Join(cacheDir, fmt.Sprintf("%s.%s", fname, ftype))
+	// NOTE: even on Win32, it's in cygwin env
+	tempFilePath := path.Join(cacheDir, fmt.Sprintf("%s.%s", fname, ftype))
 	return tempFilePath
 }
 
 func usage() {
-	fmt.Println("Please specify: center, hub or node.")
+	fmt.Println("Supported Commands: center, hub, node and reset.")
 }
 
 func startSession(name string) {
-    fmt.Printf("Starting %s...\n", name)
+    fmt.Printf("[CTMUX] Starting %s...\n", name)
 	tempConfigPath := getPath(name, "conf")
+	// Created by tmux
 	tempSockPath := getPath(name, "sock")
 
     // Write embedded config to a temporary file
-	configFileContent, err := configFiles.ReadFile(fmt.Sprintf("%s.conf", name))
-	if err != nil { panic(err) }
+	if _, err := os.Stat(tempConfigPath); os.IsNotExist(err) {
+		configFileContent, err := configFiles.ReadFile(fmt.Sprintf("%s.conf", name))
+		if err != nil { panic(err) }
 
-    if err := ioutil.WriteFile(tempConfigPath, configFileContent, 0644); err != nil {
-        panic(err)
-    }
-    defer os.Remove(tempConfigPath)
+		if err := ioutil.WriteFile(tempConfigPath, configFileContent, 0644); err != nil {
+			panic(err)
+		}
+		// defer os.Remove(tempConfigPath)
+	}
 
 	tmuxBinary, err := exec.LookPath("tmux")
 	if err != nil { panic(err) }
 
-	if _, err := os.Create(tempSockPath); err != nil {
-		panic(err)
+	if runtime.GOOS == "windows" {
+		fmt.Println("On Windows")
+
+		cmd := exec.Command("tmux", "-S", tempSockPath, "-f", tempConfigPath, "at", )
+		// Redirect stdin, stdout, and stderr to the current process's streams
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error running tmux session on Windows: %v\n", err)
+			panic(err)
+		}
+	} else {
+		// Probably use the exec.Command as well.
+		args := []string{"tmux", "-S", tempSockPath, "-f", tempConfigPath, "at"}
+		if err := syscall.Exec(tmuxBinary, args, os.Environ()); err != nil {
+			panic(err)
+		}
 	}
-    defer os.Remove(tempSockPath)
-	args := []string{"tmux", "-S", tempSockPath, "-f", tempConfigPath, "at"}
-	if err := syscall.Exec(tmuxBinary, args, os.Environ()); err != nil {
+}
+
+func resetCache() {
+    if err := os.RemoveAll(cacheDir); err != nil {
         panic(err)
-	}
+    }
+	fmt.Printf("Removed %s\n", cacheDir);
 }
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("No arguments provided.")
+		fmt.Fprintf(os.Stderr, "Error: No arguments provided.")
 		usage()
 		os.Exit(1)
 	}
@@ -68,6 +93,8 @@ func main() {
 		startSession("hub")
 	case "node":
 		startSession("node")
+	case "reset":
+		resetCache()
 	default:
 		fmt.Printf("Invalid argument %s.\n", os.Args[1])
 		usage()
